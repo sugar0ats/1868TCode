56179cf895605db0e1a9d632ef746736a96712d3
emma <emmacao1234@gmail.com>
2022-01-18 19:11:48 -0800

    added left and right motors + intake motor, 2 controllers

---
+++ src/robot-config.cpp
@@ -0,0 +1,28 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor LeftMotor = motor(PORT1, ratio18_1, false);
+motor RightMotor = motor(PORT2, ratio18_1, false);
+controller Controller1 = controller(primary);
+controller Controller2 = controller(partner);
+motor IntakeMotor = motor(PORT3, ratio18_1, false);
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
